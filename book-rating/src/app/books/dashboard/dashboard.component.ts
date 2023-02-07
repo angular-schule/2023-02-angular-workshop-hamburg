6fe20de81a05d7c9158c487e91309eda617dcbb8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-07 14:34:22 +0100

    TODO: TDD

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
@@ -9,25 +9,24 @@ import { BookRatingService } from '../shared/book-rating.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  books: Book[] = [];
+  books: Book[] = [
+    {
+      isbn: '123',
+      title: 'Angular',
+      description: 'Grundlagen und mehr',
+      price: 36.9,
+      rating: 5
+    },
+    {
+      isbn: '456',
+      title: 'Vue.js',
+      description: 'Das grüne Framework',
+      price: 32.9,
+      rating: 3
+    }
+  ];
 
   constructor(private rs: BookRatingService) {
-    this.books = [
-      {
-        isbn: '123',
-        title: 'Angular',
-        description: 'Grundlagen und mehr',
-        price: 36.9,
-        rating: 5
-      },
-      {
-        isbn: '456',
-        title: 'Vue.js',
-        description: 'Das grüne Framework',
-        price: 32.9,
-        rating: 3
-      }
-    ];
   }
 
   doRateUp(book: Book) {
