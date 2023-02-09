abfd893b1abe586483e9bc7121a87576a79df7f4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-09 11:11:06 +0100

    Hands On: create Book event

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-create',
@@ -31,4 +32,22 @@ export class BookCreateComponent {
 
   c = this.bookForm.controls;
 
+  hasError(control: FormControl) : boolean {
+    return control.touched && control.invalid;
+  }
+
+  submitForm() {
+    const newBook: Book = {
+      ...this.bookForm.getRawValue(),
+      rating: 1,
+      price: 1
+    }
+
+    // 1. Erstelle ein Event mit dem Namen "create"
+    // 2. Versende das neue Buch per Event
+    // 3. Subscribe dich auf das Event
+    // 4. Füge das neue Buch dem Buch-Array hinzu (Achtung: immutable arbeiten!)
+
+    this.bookForm.reset();
+  }
 }
