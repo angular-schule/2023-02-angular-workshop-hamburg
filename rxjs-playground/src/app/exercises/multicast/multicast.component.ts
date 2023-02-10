95735f1a53f6b3a4b49c7f4d4d6d6dce4fd25d54
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-10 10:10:46 +0100

    rxjs: muticasts

--- rxjs-playground/src/app/exercises/multicast/multicast.component.ts
+++ rxjs-playground/src/app/exercises/multicast/multicast.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil } from 'rxjs';
+import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil, shareReplay, delay } from 'rxjs';
 
 import { MeasureValuesService } from './measure-values.service';
 import { ExerciseService } from '../exercise.service';
@@ -14,11 +14,26 @@ export class MulticastComponent implements OnDestroy {
   logStream$ = new ReplaySubject<string>();
   private destroy$ = new Subject<void>();
 
-  measureValues$: Observable<number>; // später: Subject<number>;
+  measureValues$: Subject<number>;
 
   constructor(private mvs: MeasureValuesService, private es: ExerciseService) {
     /**************!!**************/
-    this.measureValues$ = this.mvs.getValues();
+
+    // 1. unchanged (cold)
+    // this.measureValues$ = this.mvs.getValues();
+
+    // 2. shares observable (multicast) --> Subject
+    // this.measureValues$ = this.mvs.getValues().pipe(share())
+
+    // 3. mit Buffer --> ReplaySubject
+    // this.measureValues$ = this.mvs.getValues().pipe(shareReplay(1))
+
+    // 4. mit BehaviourSubject (Startwert)
+
+    this.measureValues$ = new BehaviorSubject(9999);
+    this.mvs.getValues().pipe(delay(3000)).subscribe(this.measureValues$)
+
+
     /**************!!**************/
 
   }
