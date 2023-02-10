29a2437435e699517e021480fa64685175e7d37e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-10 12:16:33 +0100

    rxjs: switchMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { concatMap, map, mergeMap, switchMap } from 'rxjs';
 import { BooksService } from '../shared/http';
 
 @Component({
@@ -14,7 +14,7 @@ export class BookDetailsComponent {
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn')!),
-    map(isbn => this.bs.booksIsbnGet(isbn))
+    switchMap(isbn => this.bs.booksIsbnGet(isbn))
   )
 
 }
