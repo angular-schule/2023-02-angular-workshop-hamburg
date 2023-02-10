321664e3870f63cb99553eabbc94a47bc5c85e37
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-10 11:18:53 +0100

    rxjs: merge

--- rxjs-playground/src/app/exercises/chat/chat.component.ts
+++ rxjs-playground/src/app/exercises/chat/chat.component.ts
@@ -35,7 +35,11 @@ export class ChatComponent {
 
     /**************!!**************/
 
-     EMPTY.subscribe({                                   
+    merge(
+      this.msg.julia$,
+      this.msg.georg$,
+      this.msg.john$
+    ).subscribe({
       next: e => this.log(e),
       error: err => this.log('❌ ERROR: ' + err),
       complete: () => this.log('✅ All members left')
